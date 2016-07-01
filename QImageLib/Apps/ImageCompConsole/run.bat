8fa738b797a1a66c6308843e89fead55d9a84047
quanb <quanb@QUANBEN-ASUS>
2016-07-02 01:12:42 +1000

    updated the run script and released a new clickonce 1.2

--- QImageLib/Apps/ImageCompConsole/run.bat
+++ QImageLib/Apps/ImageCompConsole/run.bat
@@ -2,3 +2,6 @@
 @echo QImageLib-ImageComp environment has been set up.
 @echo Type in 'imagecomp --help' for help.
 @echo Note Typing in 'imagecomp' without arguments will meaninglessy open another console on top of this.
+@echo off
+cd %userprofile%
+echo on
